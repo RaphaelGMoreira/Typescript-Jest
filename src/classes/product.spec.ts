c822cc81cc6cf1c8ea3521aeb38835106ac910eb
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-05-26 08:37:10 -0300

    teste jest product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut('Camiseta', 39.9);
+
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut).toHaveProperty('price', 39.9);
+    expect(sut.price).toBeCloseTo(39.9);
+  });
+});
